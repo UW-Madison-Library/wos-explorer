bd45ca46517066dc02123542e6541052335f2de5
Steve Meyer <stephen.meyer@wisc.edu>
2018-10-19 14:25:34 -0500

    Adding logging

--- wos_explorer/__init__.py
+++ wos_explorer/__init__.py
@@ -1,1 +1,4 @@
+import logging
 from . import article_finder
+
+logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
--- wos_explorer/article_finder.py
+++ wos_explorer/article_finder.py
@@ -1,9 +1,21 @@
+import logging
 from .article import Article
 
+logger = logging.getLogger(__name__)
+
 def match_articles(input_filepath, matcher, collector):
+    count = 0
+    logger.info("Opening File: %s" % input_filepath)
     with open(input_filepath) as input_file:
         for line in input_file:
+            count += 1
             article = Article(line)
             if matcher.matches(article):
                 collector.collect(article)
+
+            if count % 100000 == 0:
+                logger.info("Articles Read in File: " + str(count))
+
+    logger.info("Total Articles Read in File: " + str(count))
+
     return collector.close()
