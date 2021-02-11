dfac67d360ffb9f2cee3937bbda5f05b0aac47f0
Stephen Meyer <stephen.meyer@wisc.edu>
2021-02-11 14:25:57 -0600

    Bump version number for citation matcher

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wos_explorer',
-      version='0.4.0',
+      version='0.5.0',
       description='Utility scripts to find article records within the Web of Science data set.',
       long_description='Explore the JSON converted data representation of the Clarivate Web of Science data set. Requires institutional access to the data set.',
       classifiers=[
@@ -10,7 +10,7 @@ setup(name='wos_explorer',
         'Programming Language :: Python 3.5',
         'Topic :: Text Processing',
       ],
-      keywords='search analysis',
+      keywords='search analysis citations',
       url='https://github.com/UW-Madison-Library/wos-explorer',
       author='UW-Madison Library Technology Group',
       author_email='ltghelpdesk@library.wisc.edu',
